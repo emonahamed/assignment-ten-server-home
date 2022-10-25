8f88a3424ab67266615d2d9f525b3fbafb387acd
emonahamed <emonahamed711@gmail.com>
2022-10-25 13:46:59 +0600

    courses added

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+const courses = require('./data/courses.json')
+
+
+
 app.get('/', (req, res) => {
     res.send('News Api Running')
 })
+
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+
+})
+
+
+
 
 app.listen(port, () => {
     console.log('news running 3', port)
