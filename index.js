d04fc7206a8432ce7c61a17607d07528be9a3f66
emonahamed <emonahamed711@gmail.com>
2022-10-25 13:50:57 +0600

    cors added

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 
-const courses = require('./data/courses.json')
+
+const courses = require('./data/courses.json');
 
 
 
@@ -16,8 +19,6 @@ app.get('/courses', (req, res) => {
     res.send(courses);
 
 })
-
-
 
 
 app.listen(port, () => {
